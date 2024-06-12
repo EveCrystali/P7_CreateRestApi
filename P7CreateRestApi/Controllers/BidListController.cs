381051b89fb64a3e9dc091d48e0e04d49071d94d
RTX4070TI <evequeenrevenge@gmail.com>
2024-06-12 16:56:01 +0200

    feat(BidList): implement all CRUD operations in BidListController.cs - implement Create (PostBidList /add) - implement Read (GetTBidList /{id} and GetBidLists /list) - implement Update (PutBidList /update/{id}) - implement Delete (DeleteBidList delete/{id})

--- P7CreateRestApi/Controllers/BidListController.cs
+++ P7CreateRestApi/Controllers/BidListController.cs
@@ -1,40 +1,101 @@
-using Dot.Net.WebApi.Domain;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Dot.Net.WebApi.Data;
+using Dot.Net.WebApi.Domain;
 
-namespace Dot.Net.WebApi.Controllers
+namespace P7CreateRestApi.Controllers
 {
-    [ApiController]
     [Route("[controller]")]
+    [ApiController]
     public class BidListController : ControllerBase
     {
-        [HttpGet]
-        [Route("validate")]
-        public IActionResult Validate([FromBody] BidList bidList)
+        private readonly LocalDbContext _context;
+
+        public BidListController(LocalDbContext context)
         {
-            // TODO: check data valid and save to db, after saving return bid list
-            return Ok();
+            _context = context;
         }
 
-        [HttpGet]
-        [Route("update/{id}")]
-        public IActionResult ShowUpdateForm(int id)
+        [HttpGet("list")]
+        public async Task<ActionResult<IEnumerable<BidList>>> GetBidLists()
         {
-            return Ok();
+            return await _context.BidLists.ToListAsync();
         }
 
-        [HttpPost]
-        [Route("update/{id}")]
-        public IActionResult UpdateBid(int id, [FromBody] BidList bidList)
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BidList>> GetBidList(int id)
         {
-            // TODO: check required fields, if valid call service to update Bid and return list Bid
-            return Ok();
+            var bidList = await _context.BidLists.FindAsync(id);
+
+            if (bidList == null)
+            {
+                return NotFound("BidList with this Id doesn't exist");
+            }
+
+            return bidList;
         }
 
-        [HttpDelete]
-        [Route("{id}")]
-        public IActionResult DeleteBid(int id)
+        [HttpPut("update/{id}")]
+        public async Task<IActionResult> PutBidList(int id, BidList bidList)
         {
-            return Ok();
+            if (id != bidList.BidListId)
+            {
+                return BadRequest("The Id entered in the parameter is not the same as the Id enter in the body");
+            }
+
+            _context.Entry(bidList).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BidListExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost("add/{id}")]
+        public async Task<ActionResult<BidList>> PostBidList(BidList bidList)
+        {
+            _context.BidLists.Add(bidList);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBidList", new { id = bidList.BidListId }, bidList);
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteBidList(int id)
+        {
+            var bidList = await _context.BidLists.FindAsync(id);
+            if (bidList == null)
+            {
+                return NotFound("BidList with this Id doesn't exist");
+            }
+
+            _context.BidLists.Remove(bidList);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BidListExists(int id)
+        {
+            return _context.BidLists.Any(e => e.BidListId == id);
         }
     }
 }
