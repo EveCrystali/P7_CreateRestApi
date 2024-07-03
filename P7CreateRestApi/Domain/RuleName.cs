1f6029c26ef08b75482942556ad057104e19e804
RTX4070TI <evequeenrevenge@gmail.com>
2024-07-03 12:29:28 +0200

    feat: add validations for all fields of domain class
    - implement DataAnnotations for all property in the domain class
    - implement DoubleValidation DataAnnotation to facilitate double property validation
    - add missing property for Trade domain class and update LocalDbContext the changes to reflect this change, ensuring the database is correctly updated
    
    resolve issues:
    - Add/Verify fields validations in forms for User #35
    - Add/Verify fields validations in forms for Trade #34
    - Add/Verify fields validations in forms for Rule #33
    - Add/Verify fields validations in forms for Rating #32
    - Add/Verify fields validations in forms for CurvePoint #31

--- P7CreateRestApi/Domain/RuleName.cs
+++ P7CreateRestApi/Domain/RuleName.cs
@@ -1,13 +1,39 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Dot.Net.WebApi.Controllers
 {
     public class RuleName
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Json { get; set; }
-        public string Template { get; set; }
-        public string SqlStr { get; set; }
-        public string SqlPart { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [DataType(DataType.Text, ErrorMessage = "Name must be a string")]
+        [MaxLength(50, ErrorMessage = "Name can't be longer than 50 characters")]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [DataType(DataType.MultilineText, ErrorMessage = "Description must be a string")]
+        [MaxLength(500, ErrorMessage = "Description can't be longer than 500 characters")]
+        public required string Description { get; set; }
+
+        [Required(ErrorMessage = "Json is required")]
+        [DataType(DataType.MultilineText, ErrorMessage = "Json must be a string")]
+        [MaxLength(5000, ErrorMessage = "Json can't be longer than 5000 characters")]
+        public required string Json { get; set; }
+
+        [Required(ErrorMessage = "Template is required")]
+        [DataType(DataType.MultilineText, ErrorMessage = "Template must be a string")]
+        [MaxLength(1000, ErrorMessage = "Template can't be longer than 1000 characters")]
+        public required string Template { get; set; }
+
+        [Required(ErrorMessage = "SqlStr is required")]
+        [DataType(DataType.MultilineText, ErrorMessage = "SqlStr must be a string")]
+        [MaxLength(1000, ErrorMessage = "SqlStr can't be longer than 1000 characters")]
+        public required string SqlStr { get; set; }
+
+        [Required(ErrorMessage = "SqlPart is required")]
+        [DataType(DataType.MultilineText, ErrorMessage = "SqlPart must be a string")]
+        [MaxLength(1000, ErrorMessage = "SqlPart can't be longer than 1000 characters")]
+        public required string SqlPart { get; set; }
     }
 }
---
+++ P7CreateRestApi/Domain/Validations/DoubleValidationAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dot.Net.WebApi.Domain;
+
+public class DoubleValidationAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value == null)
+        {
+            return new ValidationResult("Value is required.");
+        }
+
+        if (double.TryParse(value.ToString(), out _))
+        {
+        return ValidationResult.Success ?? new ValidationResult("Validation succeeded.");
+        }
+
+        return new ValidationResult("Please enter a valid double value.");
+    }
+}
