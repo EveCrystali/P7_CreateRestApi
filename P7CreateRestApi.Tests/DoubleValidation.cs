e04d45e5f42dcfb909c707c69e539391ea3fe1c3
RTX4070TI <evequeenrevenge@gmail.com>
2024-07-03 19:02:56 +0200

    test(P7CreateRestApi.Tests): add new test solution and new unit tests for DoubleValidation
    - add a new test project P7CreateRestApi.Tests referencing P7CreateRestApi
    - implement InlineData tests for DoubleValidation attribute
    
    Setting up solution for : #29 #28 #27 #26

---
+++ P7CreateRestApi.Tests/DoubleValidation.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+using Dot.Net.WebApi.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace P7CreateRestApi.Tests;
+
+public class DoubleValidationAttributeTests
+{
+    private static readonly ValidationResult ValidationSuccess = ValidationResult.Success;
+    private string ValidationFailMessage = "Please enter a valid double value.";
+
+    [Theory]
+    [InlineData("123.45", true)]
+    [InlineData("0", true)]
+    [InlineData("-123.45", true)]
+    [InlineData("abc", false)]
+    [InlineData("", false)]
+    [InlineData(null, false)]
+    public void IsValid_ShouldReturnExpectedResult(string? input, bool resultExpected)
+    {
+        // Arrange
+        var attribute = new DoubleValidationAttribute();
+        var validationContext = new ValidationContext(new object());
+
+        // Act
+        var result = attribute.GetValidationResult(input, validationContext);
+
+        // Assert
+        if (resultExpected)
+        {
+            Assert.Equal(ValidationSuccess, result);
+        }
+        else
+        {
+            Assert.NotEqual(ValidationSuccess, result);
+            Assert.Equal(ValidationFailMessage, result.ErrorMessage);
+        }
+    }
+}
