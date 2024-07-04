9c7b0c9f74d3184b45660890a2b1f61a789a69c3
RTX4070TI <evequeenrevenge@gmail.com>
2024-07-04 11:35:33 +0200

    test(UserTests): add unit tests for IsUserActive method in UserTests
    - implement unit test for  IsUserActive method in multiple scenarios
    - add data annotations to LastLoginDate field to improve validation based on unit test results

---
+++ P7CreateRestApi.Tests/UserTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using Dot.Net.WebApi.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace P7CreateRestApi.Tests;
+
+public class UserTests
+{
+    private static User user = new()
+    {
+        UserName = "username",
+        Email = "email@email",
+        Fullname = "Full name",
+    };
+    [Theory]
+    [InlineData("2024-07-04T00:00:00Z", true)] 
+    [InlineData("2020-07-04T00:00:00Z", false)]
+    [InlineData(null, false)]
+    public void Test_IsUserActive_UserLastLoginWithin2Years_ShouldReturnTrue(DateTime input, bool resultExpected)
+    {
+        // Arrange
+        user.LastLoginDate = input;
+
+        // Act
+        bool result = user.IsUserActive();
+
+        // Assert
+        if (resultExpected)
+        {
+            Assert.True(result, "User should be considered active because last login < 2 years ago");
+        }
+        else
+        {
+            Assert.False(result, "User should not be considered active because last login > 2 years ago");
+        }        
+    }
+
+    // TODO: add fields validation test for user class
+    
+
+}
