425492a0584cacc13e90b5a2eb48bf8d7270a441
RTX4070TI <evequeenrevenge@gmail.com>
2024-07-04 14:11:29 +0200

    test(UserTests): add unit tests for User fields validation
    - implement unit tests for Fullname validation in multiple scenarios
    - add ValidationHelper class with TryValidateObject method to handle object validation

--- P7CreateRestApi.Tests/UserTests.cs
+++ P7CreateRestApi.Tests/UserTests.cs
@@ -1,6 +1,6 @@
-﻿using Xunit;
+﻿using System.ComponentModel.DataAnnotations;
 using Dot.Net.WebApi.Domain;
-using System.ComponentModel.DataAnnotations;
+using P7CreateRestApi.Tests;
 
 namespace P7CreateRestApi.Tests;
 
@@ -10,10 +10,12 @@ public class UserTests
     {
         UserName = "username",
         Email = "email@email",
-        Fullname = "Full name",
+        Fullname = "FullName",
+        LastLoginDate = DateTime.UtcNow
     };
+
     [Theory]
-    [InlineData("2024-07-04T00:00:00Z", true)] 
+    [InlineData("2024-07-04T00:00:00Z", true)]
     [InlineData("2020-07-04T00:00:00Z", false)]
     [InlineData(null, false)]
     public void Test_IsUserActive_UserLastLoginWithin2Years_ShouldReturnTrue(DateTime input, bool resultExpected)
@@ -32,10 +34,37 @@ public class UserTests
         else
         {
             Assert.False(result, "User should not be considered active because last login > 2 years ago");
-        }        
+        }
     }
 
-    // TODO: add fields validation test for user class
-    
+    [Theory]
+    [InlineData("", false)]
+    [InlineData(" ", false)]
+    [InlineData(null, false)]
+    [InlineData("FullName", true)]
+    [InlineData("FullName with spaces", true)]
+    [InlineData("123", false)]
+    [InlineData("FullName 123 and spaces", false)]
+    [InlineData("!@#$%^&*()", false)]
+    [InlineData("!@#$%^&*() spaces", false)]
+    [InlineData("123 !@#$%^&*() spaces", false)]
+    [InlineData("FullnameThatIsTooLongBecauseTooMuchCharacterSuperiorTo25", false)]
+    public void Test_FullName_ShouldReturnExpectedResult(string? input, bool resultExpected)
+    {
+        // Arrange
+        user.Fullname = input;
 
+        // Act
+        bool result = ValidationHelper.TryValidateObject(user, out var validationResults);
+
+        // Assert
+        if (resultExpected)
+        {
+            Assert.True(result, "User should be valid because fullname is valid");
+        }
+        else
+        {
+            Assert.False(result, "User should not be valid because fullname is not valid");
+        }
+    }
 }
